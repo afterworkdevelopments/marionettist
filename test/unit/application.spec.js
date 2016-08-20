ea849966b816b2a41385f64f96eb04a1175bb520
Emilio Forrer <dev.emilio.forrer@gmail.com>
2016-08-20 13:08:59 -0500

    starting tests with mocha

---
+++ test/unit/application.spec.js
@@ -0,0 +1,10 @@
+describe('marionettist application', function() {
+  'use strict';
+
+  it("should pass an error if no target is passed", function(done) {
+    var hey = true;
+    expect(hey).to.be.equal(true);
+    done()
+  });
+
+});
